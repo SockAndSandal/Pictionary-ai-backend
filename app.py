862e6c7c3a4e33f6697f3881f26684540789e2b2
Aman <amanbeer899@gmail.com>
2021-04-04 04:07:32 +0530

    cors

--- app.py
+++ app.py
@@ -39,5 +39,13 @@ def predict():
     # return data
     return jsonify(results=output)
 
+@app.after_request
+def after_request(response):
+    print("log: setting cors" , file = sys.stderr)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
 if __name__ == '__main__':
     app.run(port = 5000, debug=True)
